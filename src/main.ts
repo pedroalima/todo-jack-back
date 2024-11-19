94e535bc4f730e09867266d4040e63029a6d0a3d
Pedro Lima <tsc.pedrolima@gmail.com>
2024-11-19 16:49:01 -0300

    chore: change CORS origin path

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
 
   // CORS
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'http://localhost:5000',
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
     credentials: true,
   });
